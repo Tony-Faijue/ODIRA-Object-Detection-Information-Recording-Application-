50784125f66d0ec374eea715350d83238f1a32f3
Tony Faijue <tonyf17191@gmail.com>
2025-11-28 18:54:12 -0600

    added setting service & logic to track setting data

---
+++ frontend/ODIRA/src/app/settings-service.ts
@@ -0,0 +1,50 @@
+import { Injectable, signal, Signal } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export interface Category{
+  id: string;
+  name: string;
+  checked: boolean;
+  label: string;
+}
+
+export interface SettingsData{
+  objThresh: number;
+  nmsThresh: number;
+  categories: Category[];
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SettingsService {
+
+  private initialSettings: SettingsData = {
+    objThresh: 0.5,
+    nmsThresh: 0.2,
+    categories: [
+    { id: 'vehicles', name:"vehicles" , checked: false, label:"Vehicles"},
+    { id: 'people', name:"people" , checked: false, label:"People"},
+    { id: 'clothes', name:"clothes" , checked: false, label:"Clothes & Accessories"},
+    { id: 'animals', name:"animals" , checked: false, label:"Animals"},   
+    { id: 'furniture', name:"furniture" , checked: false, label:"Furniture"},
+    { id: 'food', name:"food" , checked: false, label:"Food"},
+    { id: 'kitchen', name:"kitchen" , checked: false, label:"Kitchen"},
+    { id: 'sports', name:"sports" , checked: false, label:"Sports"},
+    { id: 'street', name:"street" , checked: false, label:"Street Items"},
+    { id: 'electronics', name:"electronics" , checked: false, label:"Electronics"},
+    { id: 'misc', name:"misc" , checked: false, label:"Miscellaneous"},
+    ]
+  };
+
+  private settingsSubject = new BehaviorSubject<SettingsData>(this.initialSettings);
+  public currentSettings: Observable<SettingsData> = this.settingsSubject.asObservable();
+
+  updateSettings(newSettings: SettingsData): void{
+    this.settingsSubject.next(newSettings);
+  }
+
+  currentSettingsSnapshot(): SettingsData {
+    return this.settingsSubject.value;
+  }
+}
--- frontend/ODIRA/src/app/settings/settings.ts
+++ frontend/ODIRA/src/app/settings/settings.ts
@@ -1,31 +1,60 @@
-import { Component, signal, Signal } from '@angular/core';
-import { FormsModule, NgModel} from '@angular/forms'
+import { Component, inject, OnInit, signal, computed, effect } from '@angular/core';
+import { FormsModule, NgModel} from '@angular/forms';
+import { Category, SettingsData, SettingsService } from '../settings-service';
+
 @Component({
   selector: 'app-settings',
   imports: [FormsModule],
   templateUrl: './settings.html',
   styleUrl: './settings.css',
 })
-export class Settings {
 
-  categories = [
-    { id: '1', name:"vehicles" , checked: false, label:"Vehicles"},
-    { id: '2', name:"people" , checked: false, label:"People"},
-    { id: 'clothes', name:"clothes" , checked: false, label:"Clothes & Accessories"},
-    { id: 'animals', name:"animals" , checked: false, label:"Animals"},   
-    { id: 'furniture', name:"furniture" , checked: false, label:"Furniture"},
-    { id: 'food', name:"food" , checked: false, label:"Food"},
-    { id: 'kitchen', name:"kitchen" , checked: false, label:"Kitchen"},
-    { id: 'sports', name:"sports" , checked: false, label:"Sports"},
-    { id: 'street', name:"street" , checked: false, label:"Street Items"},
-    { id: 'electronics', name:"electronics" , checked: false, label:"Electronics"},
-    { id: 'misc', name:"misc" , checked: false, label:"Miscellaneous"},
-  ];
+export class Settings implements OnInit{
+
+  settingsService = inject(SettingsService);
+
+  categories: Category[] = [];
 
   selectAllChecked:boolean = false;
-  objThresh:Signal<number> = signal(0.5);
-  nmsThresh:Signal<number> = signal(0.2);
 
+  //Signals fo UI display
+  objThresh = signal(0.5);
+  nmsThresh = signal(0.2);
+
+  //Use of computed signal for settings data
+  //signal updates automatically when objthresh or nmsthresh changes
+  settingsDataComputed = computed<Omit<SettingsData, 'categories'>>(() => {
+    return {
+      objThresh: this.objThresh(),
+      nmsThresh: this.nmsThresh(),
+    };
+  });
+
+  
+  constructor(){
+
+    effect(() => {
+      const currentThresholds = this.settingsDataComputed();
+      //When the thresholds changes, update the service with the latest settings data
+      this.settingsService.updateSettings({...currentThresholds, categories:this.categories.filter(s=>s.checked)});
+      console.log("Setting Service data auto updated");
+
+      //Print the current state of the settings
+      const currentState = this.settingsService.currentSettingsSnapshot();
+      console.log("Service state after threshold update:", currentState.objThresh, currentState.nmsThresh, "Threshold Values");
+    });
+
+  }
+
+  public ngOnInit(): void {
+    //Initialize local state from service snapshot 
+    const currentSettings = this.settingsService.currentSettingsSnapshot();
+
+    this.objThresh.set(currentSettings.objThresh);
+    this.nmsThresh.set(currentSettings.nmsThresh);
+    this.categories = currentSettings.categories;
+    this.updateSelectAllChecked();
+  }
 
   /**
    * Method for toggling the select all check box
@@ -40,9 +69,27 @@ export class Settings {
     let allChecked = this.categories.every(category => category.checked);
     if(allChecked == true){
       this.selectAllChecked = true;
+      this.triggerCategoryUpdate();
     } else {
       this.selectAllChecked = false;
+      this.triggerCategoryUpdate();
     }
+  }
+
+  /**
+   * Method to update categories is setting service
+   */
+  public triggerCategoryUpdate(): void{
+    const currentThresholds = this.settingsDataComputed();
+    let dataToSave: SettingsData = {
+      ...currentThresholds,
+      categories: this.categories.filter(s => s.checked)
+    };
+    this.settingsService.updateSettings(dataToSave);
+    console.log('Setting Service with latest categories');
+    //Check service state after category update
+    const currentState = this.settingsService.currentSettingsSnapshot();
+    console.log("Service state after category update: ", currentState.categories);
   }
 
 
